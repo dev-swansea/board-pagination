39fcc5979950cb8e7d9844261562299967c92a0f
swans <swansea2013@naver.com>
2023-05-01 21:02:09 +0900

    pagination 끝냈는데, 페이지 이동과 검색의 함수를 따로 분리해야 될듯

--- src/main/java/com/example/board/domain/post/PostController.java
+++ src/main/java/com/example/board/domain/post/PostController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 //i  컨트롤러의 게시글 생성/수정/삭제 메서드에서 showMessageAndRedirect( )를 호출해 주기만 하면 됩니다.
 @Controller
@@ -63,18 +65,40 @@ public class PostController {
 
   //  수정
   @PostMapping("/post/update.do")
-  public String updatePost(final PostRequest params, Model model) {
+  public String updatePost(final PostRequest params, Model model, final SearchDto queryParams) {
+    log.info("수완 queryParams => {}" + queryParams);
     service.updatePost(params);
-    MessageDto message = new MessageDto("글이 수정됨", "/post/list.do", RequestMethod.POST, null);
+    MessageDto message = new MessageDto("글이 수정됨", "/post/list.do", RequestMethod.GET, queryParamsToMap(queryParams));
     return showMessageAndRedirect(message, model);
   }
 
   // 물리적 삭제가 아닌 논리 삭제이다.
+
+  /*
+   * 기존에는 파라미터로 게시글 번호(id)만 수집했으나,
+   * 이전 페이지 정보까지 수집하기 위해 SearchDto 타입의 객체인 queryParams를 파라미터로 선언해 주었습니다.
+   * */
   @PostMapping("/post/delete.do")
-  public String deletePost(@RequestParam final Long id, Model model) {
+  public String deletePost(@RequestParam final Long id, Model model, final SearchDto queryParams) {
     service.deletePost(id);
-    MessageDto message = new MessageDto("글이 삭제됨", "/post/list.do", RequestMethod.POST, null);
+    MessageDto message = new MessageDto("글이 삭제됨", "/post/list.do", RequestMethod.GET, queryParamsToMap(queryParams));
     return showMessageAndRedirect(message, model);
+  }
+
+  // 쿼리 스트링 파라미터를 Map에 담아 반환
+  /*
+   * 해당 메서드는 deletePost( )에서 수집한 이전 페이지 정보(queryParams의 모든 멤버)를 Map에 담아 리턴해주는 역할을 하는데요.
+   * MessageDto의 생성자는 마지막 파라미터로 Map을 전달받아 객체를 생성하기 때문에 컨트롤러에 해당 메서드를 추가해 주었습니다.
+   * 참고로, 해당 기능은 컨트롤러에서 알러트 메시지 처리하기 기능의 연장선입니다. 추가적으로 뷰(HTML)로 전달할 데이터가 있다면, 유사한 방법으로 처리해 주시면 됩니다.
+   * */
+  private Map<String, Object> queryParamsToMap(final SearchDto queryParams) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("page", queryParams.getPage());
+    map.put("recordSize", queryParams.getRecordSize());
+    map.put("pageSize", queryParams.getPageSize());
+    map.put("keyword", queryParams.getKeyword());
+    map.put("searchType", queryParams.getSearchType());
+    return map;
   }
 
   //  사용자에게 메시지를 전달하고 페이지를 리다이렉트
